861efaeb6430540245fc2817b934da86f45c4e54
Puchunim <pedro_s_m_rodrigues@hotmail.com>
2022-02-27 16:32:00 -0300

    Setup information for v0.0.0

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:
+    requirements = fh.read().splitlines()
+
+setuptools.setup(
+    name="uwrapper",
+    version="0.0.0",
+    author="Pedro Spezziale",
+    author_email="pedro_s_m_rodrigues@hotmail.com",
+    description="Simple wrapper for UnionMangas.top's mangas and webtoons",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/puchunim/uwrapper",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests',
+        'lxml'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
