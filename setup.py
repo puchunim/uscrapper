cda0872ed9ff5d414e86036e246fa8f434a185ef
Puchunim <pedro_s_m_rodrigues@hotmail.com>
2022-02-28 15:15:54 -0300

    FIX: Dunno what is the problem but it's seen to be fixed

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/puchunim/uwrapper",
-    packages=setuptools.find_packages(where="uwrapper"),
+    packages=setuptools.find_packages(),
     install_requires=[
         'requests',
         'lxml'
